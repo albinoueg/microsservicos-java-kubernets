3d21d858c15048311ede948ad863d7f9f2fc9392
Albino Moreira dos Santos <albino.sistemas@gmail.com>
2021-09-22 21:43:11 -0300

    Correção

--- product-api/src/main/resources/db.migration/V3__insert_categories.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO products.category(id, nome) VALUES(1, 'Eletrônico');
-INSERT INTO products.category(id, nome) VALUES(1, 'Móveis');
-INSERT INTO products.category(id, nome) VALUES(1, 'Brinquedos');
--- product-api/src/main/resources/db/migration/V1__Create_category_table.sql
+++ product-api/src/main/resources/db/migration/V1__Create_category_table.sql
@@ -1,6 +1,6 @@
 create schema if not exists products;
 
-create table products.category(
+create table products.category (
     id bigserial primary key,
     nome varchar(100) not null
 );
